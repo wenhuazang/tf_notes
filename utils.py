bab767e9dd30bb6792d041d424897b01989cfafd
wenhuazang <wenhuazang@sohu-inc.com>
2017-11-07 11:06:15 +0800

    utils

--- utils.py
+++ utils.py
@@ -1,198 +1,114 @@
-"""
-Some codes from https://github.com/Newmu/dcgan_code
-"""
-from __future__ import division
-import math
-import json
-import random
-import pprint
-import scipy.misc
+import gzip
+import struct
+import cPickle as pkl
+import cPickle
+import tables
 import numpy as np
-from time import gmtime, strftime
+import os
+from sklearn.datasets import load_svmlight_files
 
-pp = pprint.PrettyPrinter()
 
-get_stddev = lambda x, k_h, k_w: 1/math.sqrt(k_w*k_h*x.get_shape()[-1])
+def load_amazon(input_name, data_folder=None, suffix='_train.svmlight'):
 
-def get_image(image_path, image_size, is_crop=True, resize_w=64, is_grayscale = False):
-    return transform(imread(image_path, is_grayscale), image_size, is_crop, resize_w)
+    if data_folder is None:
+        data_folder = 'data/'
+    data_file = os.path.join(data_folder, input_name + suffix)
+    print 'load data file {}'.format(data_file)
+    xs, ys = load_svmlight_files([data_file])
+    # Convert sparse matrices to numpy 2D array
+    xs = np.array(xs.todense())
+    # Convert {-1,1} labels to {0,1} labels
+    ys = np.array((ys + 1) / 2, dtype=int)
+    return xs, ys
 
-def save_images(images, size, image_path):
-    return imsave(images, size, image_path)
+def loadImageSet(filename):
+    print "load image set", filename
+    binfile = open(filename, 'rb')
+    buffers = binfile.read()
 
-def imread(path, is_grayscale = False):
-    if (is_grayscale):
-        return scipy.misc.imread(path, flatten = True).astype(np.float)
+    head = struct.unpack_from('>IIII', buffers, 0)
+    print "head,", head
+
+    offset = struct.calcsize('>IIII')
+    imgNum = head[1]
+    width = head[2]
+    height = head[3]
+    # [60000]*28*28
+    bits = imgNum * width * height
+    bitsString = '>' + str(bits) + 'B'  # like '>47040000B'
+
+    imgs = struct.unpack_from(bitsString, buffers, offset)
+
+    binfile.close()
+    imgs = np.reshape(imgs, [imgNum, 1, width * height])
+    print "load imgs finished"
+    return imgs
+
+
+def loadLabelSet(filename):
+    print "load label set", filename
+    binfile = open(filename, 'rb')
+    buffers = binfile.read()
+
+    head = struct.unpack_from('>II', buffers, 0)
+    print "head,", head
+    imgNum = head[1]
+
+    offset = struct.calcsize('>II')
+    numString = '>' + str(imgNum) + "B"
+    labels = struct.unpack_from(numString, buffers, offset)
+    binfile.close()
+    labels = np.reshape(labels, [imgNum, 1])
+
+    print 'load label finished'
+    return labels
+
+
+def load_mnist_raw(data_dir, train):
+    if train:
+        fd = os.path.join(data_dir, 'train-images-idx3-ubyte')
+        fl = os.path.join(data_dir, 'train-labels-idx1-ubyte')
     else:
-        return scipy.misc.imread(path).astype(np.float)
+        fd = os.path.join(data_dir, 't10k-images-idx3-ubyte')
+        fl = os.path.join(data_dir, 't10k-labels-idx1-ubyte')
+    images = loadImageSet(fd)  # [-1, 1, 784]
+    labels = loadLabelSet(fl)
+    return images, labels
 
-def merge_images(images, size):
-    return inverse_transform(images)
 
-def merge(images, size):
-    h, w = images.shape[1], images.shape[2]
-    img = np.zeros((h * size[0], w * size[1], 3))
-    for idx, image in enumerate(images):
-        i = idx % size[1]
-        j = idx // size[1]
-        img[j*h:j*h+h, i*w:i*w+w, :] = image
-
-    return img
-
-def imsave(images, size, path):
-    return scipy.misc.imsave(path, merge(images, size))
-
-def center_crop(x, crop_h, crop_w=None, resize_w=64):
-    if crop_w is None:
-        crop_w = crop_h
-    h, w = x.shape[:2]
-    j = int(round((h - crop_h)/2.))
-    i = int(round((w - crop_w)/2.))
-    return scipy.misc.imresize(x[j:j+crop_h, i:i+crop_w],
-                               [resize_w, resize_w])
-
-def transform(image, npx=64, is_crop=True, resize_w=64):
-    # npx : # of pixels width/height of image
-    if is_crop:
-        cropped_image = center_crop(image, npx, resize_w=resize_w)
+def load_usps(filename, split):
+    filename = os.path.join(filename, 'usps_28x28.pkl')
+    # filename = os.path.join(root, filename)
+    f = gzip.open(filename, 'rb')
+    data_set = cPickle.load(f)
+    f.close()
+    if split:
+        images = data_set[0][0]
+        labels = data_set[0][1]
     else:
-        cropped_image = image
-    return np.array(cropped_image)
-
-def inverse_transform(images):
-    return (images+1.)/2.
-
+        images = data_set[1][0]
+        labels = data_set[1][1]
+    images = images.transpose((0, 2, 3, 1)) / 127.5 - 1
+    return images, labels
 
 
-def to_json(output_path, *layers):
-    with open(output_path, "w") as layer_f:
-        lines = ""
-        for w, b, bn in layers:
-            layer_idx = w.name.split('/')[0].split('h')[1]
-
-            B = b.eval()
-
-            if "lin/" in w.name:
-                W = w.eval()
-                depth = W.shape[1]
-            else:
-                W = np.rollaxis(w.eval(), 2, 0)
-                depth = W.shape[0]
-
-            biases = {"sy": 1, "sx": 1, "depth": depth, "w": ['%.2f' % elem for elem in list(B)]}
-            if bn != None:
-                gamma = bn.gamma.eval()
-                beta = bn.beta.eval()
-
-                gamma = {"sy": 1, "sx": 1, "depth": depth, "w": ['%.2f' % elem for elem in list(gamma)]}
-                beta = {"sy": 1, "sx": 1, "depth": depth, "w": ['%.2f' % elem for elem in list(beta)]}
-            else:
-                gamma = {"sy": 1, "sx": 1, "depth": 0, "w": []}
-                beta = {"sy": 1, "sx": 1, "depth": 0, "w": []}
-
-            if "lin/" in w.name:
-                fs = []
-                for w in W.T:
-                    fs.append({"sy": 1, "sx": 1, "depth": W.shape[0], "w": ['%.2f' % elem for elem in list(w)]})
-
-                lines += """
-                    var layer_%s = {
-                        "layer_type": "fc", 
-                        "sy": 1, "sx": 1, 
-                        "out_sx": 1, "out_sy": 1,
-                        "stride": 1, "pad": 0,
-                        "out_depth": %s, "in_depth": %s,
-                        "biases": %s,
-                        "gamma": %s,
-                        "beta": %s,
-                        "filters": %s
-                    };""" % (layer_idx.split('_')[0], W.shape[1], W.shape[0], biases, gamma, beta, fs)
-            else:
-                fs = []
-                for w_ in W:
-                    fs.append({"sy": 5, "sx": 5, "depth": W.shape[3], "w": ['%.2f' % elem for elem in list(w_.flatten())]})
-
-                lines += """
-                    var layer_%s = {
-                        "layer_type": "deconv", 
-                        "sy": 5, "sx": 5,
-                        "out_sx": %s, "out_sy": %s,
-                        "stride": 2, "pad": 1,
-                        "out_depth": %s, "in_depth": %s,
-                        "biases": %s,
-                        "gamma": %s,
-                        "beta": %s,
-                        "filters": %s
-                    };""" % (layer_idx, 2**(int(layer_idx)+2), 2**(int(layer_idx)+2),
-                             W.shape[0], W.shape[3], biases, gamma, beta, fs)
-        layer_f.write(" ".join(lines.replace("'","").split()))
-
-def make_gif(images, fname, duration=2, true_image=False):
-  import moviepy.editor as mpy
-
-  def make_frame(t):
-    try:
-      x = images[int(len(images)/duration*t)]
-    except:
-      x = images[-1]
-
-    if true_image:
-      return x.astype(np.uint8)
+def load_usps_raw(data_dir, split):
+    f = tables.open_file(os.path.join(data_dir, 'usps.h5'), mode='r')
+    if split is 'train':
+        X = f.root.usps.train_X
+        y = f.root.usps.train_y
     else:
-      return ((x+1)/2*255).astype(np.uint8)
+        X = f.root.usps.test_X
+        y = f.root.usps.test_y
+    # images = []
+    # for img in X:
+    #     images.append(np.array(img, dtype=np.float64))
+    # return np.array(images), y
+    return X, y
 
-  clip = mpy.VideoClip(make_frame, duration=duration)
-  clip.write_gif(fname, fps = len(images) / duration)
 
-def visualize(sess, dcgan, config, option):
-  if option == 0:
-    z_sample = np.random.uniform(-0.5, 0.5, size=(config.batch_size, dcgan.z_dim))
-    samples = sess.run(dcgan.sampler, feed_dict={dcgan.z: z_sample})
-    save_images(samples, [8, 8], './samples/test_%s.png' % strftime("%Y-%m-%d %H:%M:%S", gmtime()))
-  elif option == 1:
-    values = np.arange(0, 1, 1./config.batch_size)
-    for idx in xrange(100):
-      print(" [*] %d" % idx)
-      z_sample = np.zeros([config.batch_size, dcgan.z_dim])
-      for kdx, z in enumerate(z_sample):
-        z[idx] = values[kdx]
-
-      samples = sess.run(dcgan.sampler, feed_dict={dcgan.z: z_sample})
-      save_images(samples, [8, 8], './samples/test_arange_%s.png' % (idx))
-  elif option == 2:
-    values = np.arange(0, 1, 1./config.batch_size)
-    for idx in [random.randint(0, 99) for _ in xrange(100)]:
-      print(" [*] %d" % idx)
-      z = np.random.uniform(-0.2, 0.2, size=(dcgan.z_dim))
-      z_sample = np.tile(z, (config.batch_size, 1))
-      #z_sample = np.zeros([config.batch_size, dcgan.z_dim])
-      for kdx, z in enumerate(z_sample):
-        z[idx] = values[kdx]
-
-      samples = sess.run(dcgan.sampler, feed_dict={dcgan.z: z_sample})
-      make_gif(samples, './samples/test_gif_%s.gif' % (idx))
-  elif option == 3:
-    values = np.arange(0, 1, 1./config.batch_size)
-    for idx in xrange(100):
-      print(" [*] %d" % idx)
-      z_sample = np.zeros([config.batch_size, dcgan.z_dim])
-      for kdx, z in enumerate(z_sample):
-        z[idx] = values[kdx]
-
-      samples = sess.run(dcgan.sampler, feed_dict={dcgan.z: z_sample})
-      make_gif(samples, './samples/test_gif_%s.gif' % (idx))
-  elif option == 4:
-    image_set = []
-    values = np.arange(0, 1, 1./config.batch_size)
-
-    for idx in xrange(100):
-      print(" [*] %d" % idx)
-      z_sample = np.zeros([config.batch_size, dcgan.z_dim])
-      for kdx, z in enumerate(z_sample): z[idx] = values[kdx]
-
-      image_set.append(sess.run(dcgan.sampler, feed_dict={dcgan.z: z_sample}))
-      make_gif(image_set[-1], './samples/test_gif_%s.gif' % (idx))
-
-    new_image_set = [merge(np.array([images[idx] for images in image_set]), [10, 10]) \
-        for idx in range(64) + range(63, -1, -1)]
-    make_gif(new_image_set, './samples/test_gif_merged.gif', duration=8)
+def load_mnist_m():
+    mnistm = pkl.load(open('/Users/paul/PycharmProjects/mnist_dan/dann/mnistm_data.pkl'))
+    mnistm_train = mnistm['train']
+    mnistm_test = mnistm['test']
+    return mnistm_train, mnistm_test
